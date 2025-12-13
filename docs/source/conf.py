30c9a419c2326b53453d4039d1a9bf9a16d0c088
Andrew Jones <ajone168@calpoly.edu>
2025-12-12 18:58:07 -0800

    Init

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))  # so `import code` works
 
 project = 'Lumache'
 copyright = '2021, Graziella'
